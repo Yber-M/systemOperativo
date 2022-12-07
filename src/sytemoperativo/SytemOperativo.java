19d81b2a2dbed9f2b10513916e5a8991608bb7a2
Yber-M} <ybermarlon@gmail.com>
2022-12-06 20:53:08 -0500

    Primer commit

---
+++ src/sytemoperativo/SytemOperativo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sytemoperativo;
+
+/**
+ *
+ * @author Yb-M
+ */
+public class SytemOperativo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
